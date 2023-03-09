c916715654b4f9be5c31d2689a4086613ee3f718
Kiki <kiki.l.walter@gmail.com>
2023-03-09 14:20:33 +0100

    removed lombok.Data in CustomerEntity

--- src/main/java/de/ebcsoft/customerapi/entity/Customer.java
+++ src/main/java/de/ebcsoft/customerapi/entity/Customer.java
@@ -4,12 +4,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Data;
 
 import java.sql.Date;
 
 @Entity
-@Data
+
 public class Customer {
 
     @Id
